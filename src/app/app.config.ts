e3c0a9400774bbdc3020d4afc04423915526abbf
MarcosRehtanz <marcos.rehtanz@gmail.com>
2023-12-07 05:47:24 -0300

    fix: import from app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { WebSocketService } from './Services/web-socket.service';
 import { provideStore } from '@ngrx/store';
-import { roomsReducer } from './store/rooms/room.reducer';
+import { roomsReducer } from './store/rooms/rooms.reducer';
 import { chatReducer } from './store/chat/chat.reducer';
 
 export const appConfig: ApplicationConfig = {
